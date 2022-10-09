38be13d46fa71214a83b69ed8e1d6ce246e1dce3
regino_pan_a_i <reginopanai@gmail.com>
2022-10-08 21:09:40 -0600

    Assignment week 4

--- mission/mission.js
+++ mission/mission.js
@@ -1,23 +1,26 @@
-const theme = document.getElementById('theme')
-theme.addEventListener("change", changeTheme);
 
-const themeSelector = getElementById('theme')// replace with code to select dropdown element out of the HTML
+
 function changeTheme() {
     //check to see what the current value of our select is. The current value is conveniently found in themeSelector.value!
     let value = themeSelector.value;
+    console.log(value)
     // if the value is dark then:
     // add the dark class to the body
     // change the source of the logo to point to the white logo.
     if (value == 'dark'){
         document.body.classList.add('dark');
-        const image = document.querySelector("#img");
-        image = <img src="byui-logo_blue.webp"></img>
+        var image = document.getElementById('logo');
+        image.src = 'byui-logo_white.png';
     }
     // otherwise
     // remove the dark class
     // make sure the logo src is the blue logo.
     else{
-        document.body.className('');
+        document.body.classList.remove('dark');
+        var image = document.getElementById('logo');
+        image.src = 'byui-logo_blue.webp';
     }
 }           
 // add eventlistener to the themeSelector element here. Use the changeTheme function as the event handler function.
+const themeSelector = document.getElementById('themes');
+themeSelector.addEventListener('change', changeTheme);
