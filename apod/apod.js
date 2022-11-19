347c51d9fce45496a7917a503109b7aa4fd6651d
regino_pan_a_i <reginopanai@gmail.com>
2022-11-19 14:50:32 -0700

    APOD

--- apod/apod.js
+++ apod/apod.js
@@ -1,4 +1,4 @@
-apodURL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+apodURL = "https://api.nasa.gov/planetary/apod?api_key=0UieBswOrvrViwGPhI6ypg1tdwR9iF8tKV6eT2yJ";
 
 async function getApod(url){
     const response = await fetch(url);
@@ -7,12 +7,15 @@ async function getApod(url){
     console.log(data);
     doStuff(data);
     }
+    else{
+        console.log('Houston, we have a problem... :/')
+    }
 }
 
 function apodTemplate(data){
     return `<section class="photo">
     <img src=${data.url} alt="${data.title}">
-    <div>
+    <div class="info">
         <h2>${data.title}</h2>
         <h3>${data.date}</h3>
         <p>${data.explanation}</p>
@@ -22,10 +25,27 @@ function apodTemplate(data){
 }
 
 function doStuff(data){
-    const outputEL = document.querySelector('main');
+    const outputEL = document.querySelector('#mainInfo');
     const html = apodTemplate(data);
     outputEL.insertAdjacentHTML('beforeend', html);
   
 }
 
 getApod(apodURL);
+
+async function getApodByDate (){
+    var date = document.querySelector('#date').value;
+    console.log(date);
+    clearContent();
+    await getApod(apodURL + `&date=${date}`);
+}
+
+function clearContent() {
+    document.querySelector('#mainInfo').innerHTML = "";
+}
+
+document.querySelector('#refresh').addEventListener('click', getApodByDate);
+
+
+
+
